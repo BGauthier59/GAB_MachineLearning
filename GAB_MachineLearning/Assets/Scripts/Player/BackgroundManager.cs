ddbb528a5f3771389310bedb558309ae9e8f89c2
BGauthier59 <g.baudinet@rubika-edu.com>
2023-03-09 20:35:17 +0100

    background auto

--- GAB_MachineLearning/Assets/Scripts/Player/BackgroundManager.cs
+++ GAB_MachineLearning/Assets/Scripts/Player/BackgroundManager.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BackgroundManager : MonoBehaviour
 {
@@ -21,22 +23,62 @@ public class BackgroundManager : MonoBehaviour
     [SerializeField] private Renderer backgroundRd;
     [SerializeField] private Renderer playgroundRd;
     [SerializeField] private float fadeDuration;
+    private bool autoChangeColor;
+    [SerializeField] private float autoColorDuration;
+    [SerializeField] private float autoColorFade;
+    [SerializeField] private Button colorButton;
 
     private void Start()
     {
         currentScheme = -1;
+        complete = true;
         ChangeColorScheme();
     }
 
-    public void ChangeColorScheme()
+    public void ChangeColor() => ChangeColorScheme();
+
+    private void ChangeColorScheme(float fade = -1f)
     {
+        if (!complete) return;
+        complete = false;
+        if ((int) fade == -1) fade = fadeDuration;
         currentScheme++;
         if (currentScheme == schemes.Length) currentScheme = 0;
         Debug.Log("Changing Color scheme");
-        //playgroundRd.material.color = schemes[currentScheme].playground;
-        playgroundRd.material.DOColor(schemes[currentScheme].playground, fadeDuration);
-        //backgroundRd.material.color = schemes[currentScheme].background;
-        backgroundRd.material.DOColor(schemes[currentScheme].background, fadeDuration);
+        playgroundRd.material.DOColor(schemes[currentScheme].playground, fade);
+        backgroundRd.material.DOColor(schemes[currentScheme].background, fade).onComplete = OnComplete;
         RenderSettings.ambientLight = schemes[currentScheme].ambientColor;
+    }
+
+    private bool complete;
+    private void OnComplete()
+    {
+        complete = true;
+    }
+
+    private void Update()
+    {
+        AutoChangeColor();
+    }
+
+    public void ToggleAutoChangeColor(bool b)
+    {
+        autoChangeColor = b;
+        colorButton.interactable = !b;
+        if (autoChangeColor) timer = autoColorDuration + autoColorFade;
+    }
+
+    private float timer;
+
+    private void AutoChangeColor()
+    {
+        if (!autoChangeColor) return;
+
+        if (timer >= autoColorDuration + autoColorFade)
+        {
+            ChangeColorScheme(autoColorFade);
+            timer = 0;
+        }
+        else timer += Time.deltaTime;
     }
 }
